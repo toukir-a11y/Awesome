87b41e938c112fbda263ffc98026d5ada53606b8
toukir <toukirhossain18@gmail.com>
2021-09-07 00:33:08 +0600

    footer.php file modified

--- footer.php
+++ footer.php
@@ -1,18 +1,17 @@
 
-<?php $new= get_field("top");?>
+<?php $footer= get_field("footer",'option');?>
 <footer id="footer">
 			<div class="container">
 				<div class="row">
 					<div class="col text-center">
 						<a class="footer-brand d-flex align-items-center justify-content-center" href="index.html">
-							<img src="<?php echo $new["fotter_logo"];?>" class="img-fluid" alt="">
-					    	<?php echo$new["fotter_title"];?>
+							<img src="<?php echo $footer['footer_logo'];?>" class="img-fluid" alt="">
+					    	<?php echo $footer['footer_title'];?>
 						</a>
 					</div>
 				</div>
 			</div>
 		</footer>
-
-	<?php wp_footer();?>
 	</body>
+	<?php wp_footer();?>
 </html>
