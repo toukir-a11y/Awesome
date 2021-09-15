f89baf4a53e7e14d4d6b6547b9d22dcd19327424
toukir <toukirhossain18@gmail.com>
2021-09-16 00:46:54 +0600

    error page complete

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php get_header();?>
-
+<?php  $four= get_field('error_page', 'option');?>
 <div class="container">
     <div class="row">
         <div class="col-md-8">
-            <h1 style= "margin-top:200px; text-align:center;color:red;">page not found</h1> 
+            <h1 style= "margin-top:200px; text-align:center;color:red;"><?php echo $four;?></h1> 
         </div>
     </div>
 </div>
